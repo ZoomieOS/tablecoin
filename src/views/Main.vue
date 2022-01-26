cdecfeb580e8679449a7ea8624a82c5d712c920a
Oleg Boreiko <iwenik@gmail.com>
2022-01-27 01:25:48 +0300

    [TC-6] Create Main page and routing

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.container
+  Table
+</template>
+
+<script>
+import Table from '../components/Table.vue'
+
+export default {
+  name: "Main",
+  components: {
+    Table,
+  },
+};
+</script>
