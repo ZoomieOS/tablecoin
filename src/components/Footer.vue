d391e2bc31d5dec68126d9bd173b3aed0fbb7958
Oleg Boreiko <iwenik@gmail.com>
2022-01-27 01:01:45 +0300

    [TC-4] Add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+footer.footer
+  .footer-inner
+    section.author
+      p My personal page:
+      a(href="https://www.linkedin.com/in/olegboreiko/")
+        i.fa-brands.fa-linkedin
+      a(href="https://www.facebook.com/therealzoomie/")
+        i.fa-brands.fa-facebook-square
+      a(href="https://github.com/ZoomieOS")
+        i.fa-brands.fa-github-square
+</template>
