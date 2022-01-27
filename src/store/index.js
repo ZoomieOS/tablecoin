0bd659bded59fa96e7987fe2038f686a40181242
Oleg Boreiko <iwenik@gmail.com>
2022-01-27 18:15:12 +0300

    [TC-14] Get API data in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
+import axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    coins: [],
+  },
+  getters: {
+    relatedCoins(state) {
+      return state.coins.slice(0, 3);
+    },
+  },
+  mutations: {
+    SET_COIN(state, items) {
+      state.coins = items;
+    },
+  },
+  actions: {
+    getCoins({ commit }) {
+      axios.get("https://api.coincap.io/v2/assets").then((response) => {
+        commit("SET_COIN", response.data.data);
+      });
+    },
+  },
 });
