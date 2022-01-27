56d0468cc6fc611d04d9b74e6b351c0d8df2ab17
Oleg Boreiko <iwenik@gmail.com>
2022-01-28 02:06:28 +0300

    [TC-18] Add Top coins in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,21 +4,36 @@ header
     i.fas.fa-bars
   .container
     nav.main-nav
-      img.logo(src="../assets/logo.png", alt="CoinTable")
+      img.logo(src="../assets/logo.png" alt="CoinTable")
       ul.main-menu
-        li
-          a(href="#") Bitcoin 134,32 USD
-        li
-          a(href="#") Etherium 12,32 USD
-        li
-          a(href="#") Tether 1 USD
+        li(v-for="item in relatedCoins" :key="item.id")
+          router-link(:to="{\
+          name: 'Coin',\
+          params: { name: item.name.toLowerCase() },\
+          }")
+            | {{
+            | item.name + " - " + optimizeNumber(item.priceUsd) + " USD"
+            | }}
         li
           a(href="#")
             | Portfolio: 134,32 USD
             span(style="color: green") +2,38
-            |
-            | (1,80%)
+            |               (1,80%)
 </template>
+
+<script>
+import optimizeNumber from "./mixins/optimizeNumber";
+
+export default {
+  name: "Header",
+  mixins: [optimizeNumber],
+  computed: {
+    relatedCoins() {
+      return this.$store.getters.relatedCoins;
+    },
+  },
+};
+</script>
 
 <style lang="sass">
 .main-nav
