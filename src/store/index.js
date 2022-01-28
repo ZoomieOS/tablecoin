ea5b30aa1aad834e2f327fe25d912c4cf08cf4a7
Oleg Boreiko <iwenik@gmail.com>
2022-01-28 23:45:20 +0300

    [TC-24] Create coin info paeg and store this

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     coins: [],
+    coinsFullInfo: [],
+    coinHistory: [],
   },
   getters: {
     relatedCoins(state) {
@@ -17,12 +19,32 @@ export default new Vuex.Store({
     SET_COIN(state, items) {
       state.coins = items;
     },
+    SET_COIIN_FULL(state, items) {
+      state.coinsFullInfo = items;
+    },
+    SET_HISTORY(state, items) {
+      state.coinHistory = items;
+    },
   },
   actions: {
     getCoins({ commit }) {
       axios.get("https://api.coincap.io/v2/assets").then((response) => {
         commit("SET_COIN", response.data.data);
       });
+    },
+    getCoinFullInfo({ commit }, payload) {
+      axios
+        .get(`https://api.coincap.io/v2/assets/${payload}`)
+        .then((response) => {
+          commit("SET_COIIN_FULL", response.data.data);
+        });
+    },
+    getHistory({ commit }, payload) {
+      axios
+        .get(`https://api.coincap.io/v2/assets/${payload}/history?interval=d1`)
+        .then((res) => {
+          commit("SET_HISTORY", res.data.data);
+        });
     },
   },
 });
--- src/views/Coin.vue
+++ src/views/Coin.vue
@@ -1,19 +1,53 @@
 <template lang="pug">
-section.coin-info
-  h1 Bitcoin
-  .coin-cards
-    div
-      h3 Price
-      span $716.72b
-    div
-      h3 Volume (24Hr)
-      span $716.72b
-    div
-      h3 Supply
-      span $716.72b
-    div
-      h3 Market Cap
-      span $716.72b
-  h2 History:
-  svg#line-chart(width="960" height="500")
+.container
+  section.coin-info
+    h1 {{ coinFullInfo.name }}
+    .coin-cards
+      div
+        h3 Price
+        span {{ "$" + optimizeNumber(coinFullInfo.priceUsd) }}
+      div
+        h3 Volume (24Hr)
+        span {{ "$" + optimizeNumber(coinFullInfo.volumeUsd24Hr) }}
+      div
+        h3 Supply
+        span {{ "$" + optimizeNumber(coinFullInfo.supply) }}
+      div
+        h3 Market Cap
+        span {{ "$" + optimizeNumber(coinFullInfo.marketCapUsd) }}
+      div
+        h3 Change (24Hr)
+        span
+          | {{
+          | optimizeNumber(coinFullInfo.changePercent24Hr) + "%"
+          | }}
+    h2 History:
+    chart(:linedata="coinHistory")
 </template>
+
+<script>
+import optimizeNumber from "../components/mixins/optimizeNumber";
+import chart from "../components/Chart.vue";
+
+export default {
+  name: "Coin",
+  mixins: [optimizeNumber],
+  components: {
+    chart,
+  },
+  props: ["name", "coinsInfo"],
+  beforeUpdate() {
+    this.$store.dispatch("getCoinFullInfo", this.name);
+  },
+  mounted() {
+  },
+  computed: {
+    coinFullInfo() {
+      return this.$store.state.coinsFullInfo;
+    },
+    coinHistory() {
+      return this.$store.state.coinHistory;
+    },
+  },
+};
+</script>
