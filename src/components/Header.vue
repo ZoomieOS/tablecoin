c9c1d34f131bbc541864b286f08760d88051c386
Oleg Boreiko <iwenik@gmail.com>
2022-01-28 23:49:26 +0300

    [TC-27] Remove header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,16 +4,18 @@ header
     i.fas.fa-bars
   .container
     nav.main-nav
-      img.logo(src="../assets/logo.png" alt="CoinTable")
+      router-link(:to="{name: 'Main'}")
+        img.logo(src="../assets/logo.png" alt="CoinTable")
       ul.main-menu
         li(v-for="item in relatedCoins" :key="item.id")
           router-link(:to="{\
           name: 'Coin',\
-          params: { name: item.name.toLowerCase() },\
+          params: {\
+          name: item.name.toLowerCase(),\
+          coinsInfo: item,\
+          },\
           }")
-            | {{
-            | item.name + " - " + optimizeNumber(item.priceUsd) + " USD"
-            | }}
+            | {{ item.name + " - " + optimizeNumber(item.priceUsd) + " USD" }}
         li
           a(href="#")
             | Portfolio: 134,32 USD
@@ -34,40 +36,3 @@ export default {
   },
 };
 </script>
-
-<style lang="sass">
-.main-nav
-  display: flex
-  align-items: center
-  justify-content: space-between
-  height: 60px
-  padding: 20px 0
-  font-size: 13px
-
-.logo
-  width: 160px
-
-ul
-  display: flex
-
-  li
-    padding: 0 10px
-
-    a
-      padding-bottom: 2px
-
-      &:hover
-        border-bottom: 2px solid #262626
-
-    &.main-menu
-      flex: 1
-      margin-left: 20px
-
-.menu-btn
-  cursor: pointer
-  position: absolute
-  top: 20px
-  right: 30px
-  z-index: 2
-  display: none
-</style>
